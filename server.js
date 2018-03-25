53c55e5186426b371f2d1659bd83d84440298a9b
christylaguardia <christinelaguardia@gmail.com>
2018-03-24 20:43:25 -0700

    testing routes

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const bodyParser = require('body-parser').json();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 app.get('/', bodyParser, (req, res) => {
-  res.send({ received: true });
+  res.send('received get request');
 });
 
 app.post('/', bodyParser, (req, res) => {
-  res.send({ received: true });
+  // res.send({ received: true });
+  res.send('received post request');
 });
 
 app.listen(PORT, () => {
